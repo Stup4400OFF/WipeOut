6e234b68cc7e7eb67763485332fe2306cd822551
Stup4400 <145925839+Stup4400OFF@users.noreply.github.com>
2026-02-16 14:39:25 -0500

    Delete 0.1 directory

--- 0.1/core/VAR.py
+++
@@ -1,6 +0,0 @@
-quotes = [
-    '“Two things are infinite: the universe and human stupidity; and I`m not sure about the universe.” ― Albert Einstein',
-    '“Be who you are and say what you feel, because those who mind don`t matter, and those who matter don`t mind.” ― Bernard M. Baruch',
-    '“You know you`re in love when you can`t fall asleep because reality is finally better than your dreams.” ― Dr. Seuss',
-    '“You only live once, but if you do it right, once is enough.” ― Mae West'
-]
